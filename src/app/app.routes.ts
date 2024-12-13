886ea8439834131f0784cc731752111eb9d9ae0e
LockHart5836 <202211304@gordoncollege.edu.ph>
2024-12-13 22:20:26 +0800

    Fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { ViewComponent } from './view/view.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'sidenav', component: SidenavComponent },
-    { path: 'view', component: ViewComponent }
   ];
   
