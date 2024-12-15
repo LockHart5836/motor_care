45b48326009adeb17e7693b1db0dfd5038eb8c05
LockHart5836 <202211304@gordoncollege.edu.ph>
2024-12-15 20:38:13 +0800

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
+import { CommonModule } from '@angular/common';
 import { AuthenticationService } from './services/authentication.service'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
